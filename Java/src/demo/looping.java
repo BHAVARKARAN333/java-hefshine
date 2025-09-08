2283e2e5d7fef26553a2a4b9bdfdee2d5e6e9040
Bhavar Karan <bhavarkaran333@gmail.com>
2025-09-08 12:41:25 +0530

    change

---
+++ Java/src/demo/looping.java
@@ -0,0 +1,17 @@
+package demo;
+
+public class looping {
+	public static void main(String[] args) {
+		
+		for(int i=1; i<=10; i=i+2) {
+			System.out.println(i);
+		}
+		System.out.println("---------");
+		for(int i=1; i<=10; i=i+3) {
+			System.out.println(i);
+		}
+		
+				
+		
+	}
+}
