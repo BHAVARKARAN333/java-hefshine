8519aa30c6ca3af7cd93add57dca85958ecbb1ff
Bhavar Karan <bhavarkaran333@gmail.com>
2025-09-08 12:57:14 +0530

    factorial

--- Java/src/demo/sum_of_number.java
+++ Java/src/demo/sum_of_number.java
@@ -2,25 +2,29 @@ package demo;
 
 public class sum_of_number {
 public static void main(String[] args) {
-		int sum=0;
-		for(int i=1; i<=10; i++) { 
-			//1<=10 2<=10 3<=10 4<=10 5<=10 6<=10 7<=10 8<=10 9<=10 10<=10 11<=10f
-			
-			sum= sum+i;
-		}
+//		int sum=0;
+//		for(int i=1; i<=10; i++) { 
+//			//1<=10 2<=10 3<=10 4<=10 5<=10 6<=10 7<=10 8<=10 9<=10 10<=10 11<=10f
+//			
+//			sum= sum+i;
+//		}
+//		
+//	System.out.println(sum);
+	
+	int sum=1;
+	for(int i=1; i<=5; i++) { 
+		//1<=5 2<=5 3<=5 4<=10 5<=5 6<=5f
 		
-	System.out.println(sum);
+		sum= sum*i;
+	}
+	
+System.out.println(sum);
 				
 		
 	}
 }
-// sum = 0+1 = 1
-//sum = 1+2 = 3
-//sum = 3+3 = 6
-//sum = 6+4 = 10
-//sum = 10+5 = 15
-//sum = 15+6 = 21
-//sum = 21+7 = 28
-//sum = 28+8 = 36
-//sum = 36+9 = 45
-//sum = 45+10 = 55
+// sum = 1*1 = 1
+//sum = 1*2 = 2
+//sum = 2*3 = 6
+//sum = 6*4 = 24
+//sum = 24*5 = 120
