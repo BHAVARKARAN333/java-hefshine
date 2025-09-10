67d84a13042702a4dcfb0aa909a2bc7ceaedd84b
Bhavar Karan <bhavarkaran333@gmail.com>
2025-09-10 11:41:05 +0530

    prime

---
+++ Java/src/demo/prime_number.java
@@ -0,0 +1,24 @@
+package demo;
+import java.util.*;
+
+public class prime_number {
+	public static void main(String[] args) {
+		
+	Scanner sc=new Scanner(System.in);
+	int num = sc.nextInt();
+		
+		int c=0;
+		for(int i = 1; i<=num; i++) {
+			if(num%i==0) {
+				c++;
+			}
+		}
+		if(c==2) {
+			System.out.println("prime");
+		}else {
+			System.out.println("odd");
+		
+
+	}
+	}
+}
