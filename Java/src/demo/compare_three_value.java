596b293af2410b4efa245d572ee21dbc93b8caa5
Bhavar Karan <bhavarkaran333@gmail.com>
2025-09-10 13:51:46 +0530

    update

---
+++ Java/src/demo/compare_three_value.java
@@ -0,0 +1,28 @@
+package demo;
+
+public class compare_three_value {
+	public static void main(String[] args) {
+
+
+		int a=6545, b=541, c=544;
+
+		if(a>=b) {
+			if(a>=c) {
+				System.out.println(" a is greater");
+			} else {
+				System.out.println("c is greater");
+			}
+		}
+	
+	else {
+		if(b>=c) {
+			System.out.println("b is grater");
+		}else {
+			System.out.println("c is grater");
+		}
+
+
+	}
+
+}
+}
