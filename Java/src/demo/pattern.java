e266bdc8a396e9f396afd23bf08d666d019ac959
Bhavar Karan <bhavarkaran333@gmail.com>
2025-09-11 17:56:19 +0530

    UH

---
+++ Java/src/demo/pattern.java
@@ -0,0 +1,63 @@
+package demo;
+
+public class pattern {
+	public static void main(String[] args) {
+		
+//	for(int i=1; i<=5; i++) {       //1<=5 2<=5 3<=5 4<=5 5<=5 6<=5F
+//			
+//		for(int j=1; j<=5; j++) {   //1<=5 2<=5 3<=5 4<=5 5<=5 6<=5F
+//			System.out.print("*");
+//		}
+//		System.out.println();
+//	}
+	
+		
+	
+		for(int i=5; i>=1; i--) {             
+			for(int j=1; j<=i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		
+		
+		
+	
+//		for(int i=1; i<=5; i++) {
+//			
+//			for(int j=i; j<5; j++) {
+//				System.out.print(" ");
+//				
+//			}
+//			
+//			for (int k=1; k<=(2*i-1); k++) {
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//		}
+		
+		
+		
+//		int rows = 5;
+//
+//        for (int i = 1; i <= rows; i++) {
+//            // print left spaces
+//            for (int j = rows; j > i; j--) {
+//                System.out.print(" ");
+//            }
+//            // print stars
+//            for (int k = 1; k <= i; k++) {
+//                System.out.print("* ");
+//            }
+//            System.out.println();
+//        }
+		
+		
+		
+		
+		
+		
+	}
+
+}
