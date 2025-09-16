a4b400f1084027215746e4034d1f45f8f52dc59e
Bhavar Karan <bhavarkaran333@gmail.com>
2025-09-16 10:21:22 +0530

    hu

---
+++ Java/src/demo/while_loop.java
@@ -0,0 +1,42 @@
+package demo;
+
+public class while_loop {
+
+//  int num =345;
+//  System.out.println(num %10);
+//  int num1=num/10;
+//  System.out.println(num1%10);
+//  int num2=num1/10;
+//  System.out.println(num2%10);
+
+//  int i = 1;
+//  while(i<=5){
+//      System.out.println("karan");
+//      i++;
+//  }
+
+//  int i=1;
+//  while(i<=10){
+//      System.out.println(i);
+//      i++;
+//  }
+
+//  int i=10;
+//  while(i>=1){
+//      System.out.println(i);
+//  
+//      i--;
+//  }
+//  
+  
+  int k=1;
+  while(i<=10){
+      System.out.println(k*5);
+      k++;
+  }
+  
+  
+}
+
+
+}
