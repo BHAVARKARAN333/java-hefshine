1a82f043dc792c7ee74175f8fa8f567718941db2
Bhavar Karan <bhavarkaran333@gmail.com>
2025-09-19 12:17:10 +0530

    update

---
+++ Java/src/demo/char_pattern.java
@@ -0,0 +1,53 @@
+package demo;
+
+class char_pattern {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		
+		for(int i=65; i<=69; i++) {
+			for(int j=69; j>=i; j--){
+				System.out.print(" ");
+			}
+			for(int k=65; k<=i; k++) {
+				System.out.print(" "+(char)k);
+			}
+			System.out.println();
+		}
+		
+//		
+		
+		
+//		for(int i=65; i<=69; i++) {
+//			for(int j=65; j<=i; j++){
+//				System.out.print("-");
+//			}
+//			for(int k=i; k<=69; k++) {
+//				System.out.print((char)k);
+//			}
+//			System.out.println();
+//		}
+//		
+		
+		
+		
+		for(int i=69; i>=65; i--) {
+			for(int j=69; j>=i; j--){
+				System.out.print(" ");
+			}
+			for(int k=65; k<=i; k++) {
+				System.out.print(" "+(char)k);
+			}
+			System.out.println();
+		}
+		
+		
+		
+		
+		
+
+	}
+
+}
