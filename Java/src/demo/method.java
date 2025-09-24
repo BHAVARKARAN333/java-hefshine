9762a61a7d5aaf04baf89039b8c9a0cbe1d304bd
Bhavar Karan <bhavarkaran333@gmail.com>
2025-09-24 12:01:14 +0530

    print name

---
+++ Java/src/demo/method.java
@@ -0,0 +1,23 @@
+package demo;
+
+class Task1{
+	
+	void show() {
+		System.out.println("karan");
+	}
+	
+	void data() {
+		System.out.println("bhavar");
+	}
+	
+}
+public class method {
+	public static void main(String args[]) {
+		Task1 t1 = new Task1();
+		t1.show();
+		
+		t1.data();
+		
+	}
+
+}
