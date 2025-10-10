0eb529edc5176b077271148b7d8c914c049013bd
Bhavar Karan <bhavarkaran333@gmail.com>
2025-10-10 11:45:12 +0530

    update

--- Java/src/demo/abstraction.java
+++ Java/src/demo/abstraction.java
@@ -1,19 +1,24 @@
 package demo;
 
-abstract class test12{
-	abstract void show();
-	void get()
+abstract class test12{     // can't create abject
+	abstract void add();    
+	abstract void show();  // abstract method(non method body)
+	void get()             // non-abstract method(with method body)
 {
 		System.out.println("get");}
 }
 
-class test13 extends test12{
+class test13 extends test12{    // child for implementations
 	void display() {
 		System.out.println("display");
 	}
+	 
+	void show() {                  // body of abstract method
+		System.out.println("show");        
+	}
 	
-	void show() {
-		System.out.println("show");
+	void add() {
+		System.out.println("add");
 	}
 }
 
@@ -26,6 +31,7 @@ public class abstraction {
 		t1.get();
 		t1.display();
 		t1.show();
+		t1.add();
 	}
 
 }
